61fd38935bd8a9195cdf4faffd27457f026ea817
duyanh <mdkthemoe@gmail.com>
2022-04-23 17:05:16 +0700

    sửa một ít v3

---
+++ database/seeders/SqlFileSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class SqlFileSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        DB::unprepared(File::get('database/seeders/db.sql'));
+    }
+}
